19272b77c01ac3a790408eaf23bb423c11463b71
mosm <airman515@gmail.com>
2022-07-26 23:24:22 +0900

    stdlib(math): implement random and randomseed

--- src/stdlib/math.rs
+++ src/stdlib/math.rs
@@ -1,11 +1,13 @@
 use super::{get_integer_arg, get_number_arg};
 use crate::{
     gc::GcHeap,
-    types::{Integer, NativeFunction, Number, StackWindow, Table, Type, Value},
+    types::{Integer, NativeClosure, NativeFunction, Number, StackWindow, Table, Type, Value},
     vm::{ErrorKind, Vm},
 };
 use bstr::B;
-use rand::Rng;
+use rand::{Rng, RngCore, SeedableRng};
+use rand_xoshiro::Xoshiro256StarStar;
+use std::{cell::RefCell, ops::DerefMut, rc::Rc, time::SystemTime};
 
 pub fn create_table(heap: &GcHeap) -> Table {
     let mut table = Table::new();
@@ -25,10 +27,76 @@ pub fn create_table(heap: &GcHeap) -> Table {
         std::f64::consts::PI as Number,
     );
     table.set_field(heap.allocate_string(B("rad")), NativeFunction::new(rad));
+
+    fn seed1() -> i64 {
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap_or_default()
+            .as_secs() as i64
+    }
+    let seed2 = rand::thread_rng().gen_range(1..=i64::MAX);
+
+    let rng = rng_from_seeds(seed1(), seed2);
+    let rng = Rc::new(RefCell::new(rng));
+    {
+        let rng = rng.clone();
+        table.set_field(
+            heap.allocate_string(B("random")),
+            heap.allocate(NativeClosure::new(move |vm, window| {
+                let stack = vm.stack_mut(window);
+                let mut rng = rng.borrow_mut();
+                stack[0] = match stack.len() {
+                    1 => rng.gen::<Number>().into(),
+                    2 => {
+                        let upper = get_integer_arg(stack, 1)?;
+                        if upper == 0 {
+                            rng.gen::<Integer>().into()
+                        } else {
+                            random_in_range(rng.deref_mut(), 1, upper).into()
+                        }
+                    }
+                    3 => {
+                        let lower = get_integer_arg(stack, 1)?;
+                        let upper = get_integer_arg(stack, 2)?;
+                        random_in_range(rng.deref_mut(), lower, upper).into()
+                    }
+                    _ => {
+                        return Err(ErrorKind::ExplicitError(
+                            "wrong number of arguments".to_owned(),
+                        ))
+                    }
+                };
+                Ok(1)
+            })),
+        );
+    }
     table.set_field(
-        heap.allocate_string(B("random")),
-        NativeFunction::new(random),
+        heap.allocate_string(B("randomseed")),
+        heap.allocate(NativeClosure::new(move |vm, window| {
+            let stack = vm.stack(window.clone());
+            let (x, y) = match stack.len() {
+                1 => (seed1(), seed2),
+                2 => {
+                    let x = get_integer_arg(stack, 1)?;
+                    (x, 0)
+                }
+                3.. => {
+                    let x = get_integer_arg(stack, 1)?;
+                    let y = get_integer_arg(stack, 2)?;
+                    (x, y)
+                }
+                _ => unreachable!(),
+            };
+            *rng.borrow_mut() = rng_from_seeds(x, y);
+
+            let window = vm.ensure_stack(window, 2);
+            let stack = vm.stack_mut(window);
+            stack[0] = x.into();
+            stack[1] = y.into();
+            Ok(2)
+        })),
     );
+
     table.set_field(heap.allocate_string(B("sin")), NativeFunction::new(sin));
     table.set_field(heap.allocate_string(B("sqrt")), NativeFunction::new(sqrt));
     table.set_field(heap.allocate_string(B("tan")), NativeFunction::new(tan));
@@ -124,27 +192,40 @@ fn rad(vm: &mut Vm, window: StackWindow) -> Result<usize, ErrorKind> {
     Ok(1)
 }
 
-fn random(vm: &mut Vm, window: StackWindow) -> Result<usize, ErrorKind> {
-    let mut rng = rand::thread_rng();
-    let stack = vm.stack_mut(window);
-    stack[0] = match stack.len() {
-        1 => rng.gen::<Number>().into(),
-        2 => {
-            let upper = get_integer_arg(stack, 1)?;
-            rng.gen_range(1..=upper).into()
+fn rng_from_seeds(n1: i64, n2: i64) -> Xoshiro256StarStar {
+    let mut seed = [0u8; 32];
+    seed[..8].copy_from_slice(&n1.to_le_bytes());
+    seed[8..16].copy_from_slice(&0xffi64.to_le_bytes());
+    seed[16..24].copy_from_slice(&n2.to_le_bytes());
+    let mut rng = Xoshiro256StarStar::from_seed(seed);
+    for _ in 0..16 {
+        rng.next_u64();
+    }
+    rng
+}
+
+fn random_in_range<R: Rng>(rng: &mut R, lower: Integer, upper: Integer) -> Integer {
+    fn project<R: Rng>(rng: &mut R, range: Integer) -> Integer {
+        if range & (range + 1) == 0 {
+            return rng.gen::<Integer>() & range;
         }
-        3 => {
-            let lower = get_integer_arg(stack, 1)?;
-            let upper = get_integer_arg(stack, 2)?;
-            rng.gen_range(lower..=upper).into()
+
+        let mut lim = range;
+        lim |= lim >> 1;
+        lim |= lim >> 2;
+        lim |= lim >> 4;
+        lim |= lim >> 8;
+        lim |= lim >> 16;
+        lim |= lim >> 32;
+
+        loop {
+            let rand = rng.gen::<Integer>() & lim;
+            if rand <= range {
+                return rand;
+            }
         }
-        _ => {
-            return Err(ErrorKind::ExplicitError(
-                "wrong number of arguments".to_owned(),
-            ))
-        }
-    };
-    Ok(1)
+    }
+    lower + project(rng, upper - lower)
 }
 
 fn sin(vm: &mut Vm, window: StackWindow) -> Result<usize, ErrorKind> {
