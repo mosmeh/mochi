7ec593ca2b13caf764f610d668257eea266f262d
mosm <airman515@gmail.com>
2022-08-06 01:29:56 +0900

    bin(compile): add list option

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use anyhow::Result;
-use bstr::{ByteVec, B};
+use bstr::{ByteSlice, ByteVec, B};
 use clap::{Parser, Subcommand};
 use mochi_lua::{
     gc::GcHeap,
-    types::{Integer, Table},
-    vm::Vm,
+    types::{Integer, LineRange, LuaClosureProto, Table, UpvalueDescription, Value},
+    vm::{OpCode, Vm},
 };
 use rustyline::{error::ReadlineError, Editor};
 use std::{fs::File, io::BufWriter, ops::Deref, path::PathBuf};
@@ -40,9 +40,17 @@ struct CompileCommand {
     #[clap(value_parser)]
     filename: PathBuf,
 
+    /// List (use -l -l for full listing)
+    #[clap(parse(from_occurrences), short)]
+    list: usize,
+
     /// Output to file <OUTPUT>
     #[clap(value_parser, short, default_value = "luac.out")]
     output: PathBuf,
+
+    /// Parse only
+    #[clap(value_parser, short)]
+    parse_only: bool,
 }
 
 struct LeakingGcHeap(GcHeap);
@@ -131,7 +139,188 @@ fn eval(vm: &mut Vm, line: &str) -> Result<()> {
 
 fn compile(heap: &GcHeap, command: CompileCommand) -> Result<()> {
     let proto = mochi_lua::load_file(heap, command.filename)?;
+
+    if command.list > 0 {
+        let mut stdout = std::io::stdout().lock();
+        dump_proto(&mut stdout, &proto, command.list > 1)?;
+    }
+    if command.parse_only {
+        return Ok(());
+    }
+
     let mut writer = BufWriter::new(File::create(command.output)?);
     mochi_lua::binary_chunk::dump(&mut writer, &proto)?;
+    Ok(())
+}
+
+fn dump_proto(w: &mut impl std::io::Write, proto: &LuaClosureProto, verbose: bool) -> Result<()> {
+    fn format_counter(word: &str, n: usize) -> String {
+        format!("{} {}{}", n, word, if n == 1 { "" } else { "s" })
+    }
+
+    let source = proto.source.as_bstr();
+    match &proto.lines_defined {
+        LineRange::File => write!(w, "main <{}:0,0> ", source)?,
+        LineRange::Lines(range) => write!(
+            w,
+            "function <{}:{},{}> ",
+            source,
+            range.start(),
+            range.end()
+        )?,
+    };
+    writeln!(
+        w,
+        "({})\n{}, {}, {}, {}",
+        format_counter("instruction", proto.code.len()),
+        format_counter("slot", proto.max_stack_size as usize),
+        format_counter("upvalue", proto.upvalues.len()),
+        format_counter("constant", proto.constants.len()),
+        format_counter("function", proto.protos.len())
+    )?;
+
+    for (i, insn) in proto.code.iter().enumerate() {
+        let opcode = insn.opcode();
+        write!(w, "\t{}\t{:9}\t", i + 1, opcode)?;
+        match opcode {
+            OpCode::Return0 => w.write_all(b"\n")?,
+            OpCode::LoadKX
+            | OpCode::LoadFalse
+            | OpCode::LFalseSkip
+            | OpCode::LoadTrue
+            | OpCode::Close
+            | OpCode::Tbc
+            | OpCode::Return1
+            | OpCode::VarArgPrep => writeln!(w, "{}", insn.a())?,
+            OpCode::Jmp => writeln!(w, "{}", insn.sj())?,
+            OpCode::ExtraArg => writeln!(w, "{}", insn.ax())?,
+            OpCode::Test => writeln!(w, "{} {}", insn.a(), insn.k() as u8)?,
+            OpCode::Move
+            | OpCode::LoadNil
+            | OpCode::GetUpval
+            | OpCode::SetUpval
+            | OpCode::Unm
+            | OpCode::BNot
+            | OpCode::Not
+            | OpCode::Len
+            | OpCode::Concat => writeln!(w, "{} {}", insn.a(), insn.b())?,
+            OpCode::LoadK
+            | OpCode::Closure
+            | OpCode::ForLoop
+            | OpCode::ForPrep
+            | OpCode::TForPrep
+            | OpCode::TForLoop => writeln!(w, "{} {}", insn.a(), insn.bx())?,
+            OpCode::LoadI | OpCode::LoadF => writeln!(w, "{} {}", insn.a(), insn.sbx())?,
+            OpCode::TForCall | OpCode::VarArg => writeln!(w, "{} {}", insn.a(), insn.c())?,
+            OpCode::Eq | OpCode::Lt | OpCode::Le | OpCode::EqK | OpCode::TestSet => {
+                writeln!(w, "{} {} {}", insn.a(), insn.b(), insn.k() as u8)?
+            }
+            OpCode::EqI | OpCode::LtI | OpCode::LeI | OpCode::GtI | OpCode::GeI => {
+                writeln!(w, "{} {} {}", insn.a(), insn.sb(), insn.k() as u8)?
+            }
+            OpCode::GetTabUp
+            | OpCode::GetTable
+            | OpCode::GetI
+            | OpCode::GetField
+            | OpCode::NewTable
+            | OpCode::AddK
+            | OpCode::SubK
+            | OpCode::MulK
+            | OpCode::ModK
+            | OpCode::PowK
+            | OpCode::DivK
+            | OpCode::IDivK
+            | OpCode::BAndK
+            | OpCode::BOrK
+            | OpCode::BXorK
+            | OpCode::Add
+            | OpCode::Sub
+            | OpCode::Mul
+            | OpCode::Mod
+            | OpCode::Pow
+            | OpCode::Div
+            | OpCode::IDiv
+            | OpCode::BAnd
+            | OpCode::BOr
+            | OpCode::BXor
+            | OpCode::Shl
+            | OpCode::Shr
+            | OpCode::MmBin
+            | OpCode::Call
+            | OpCode::SetList => writeln!(w, "{} {} {}", insn.a(), insn.b(), insn.c())?,
+            OpCode::MmBinK => writeln!(
+                w,
+                "{} {} {} {}",
+                insn.a(),
+                insn.b(),
+                insn.c(),
+                insn.k() as u8
+            )?,
+            OpCode::MmBinI => writeln!(
+                w,
+                "{} {} {} {}",
+                insn.a(),
+                insn.sb(),
+                insn.c(),
+                insn.k() as u8
+            )?,
+            OpCode::AddI | OpCode::ShrI | OpCode::ShlI => {
+                writeln!(w, "{} {} {}", insn.a(), insn.b(), insn.sc())?
+            }
+            OpCode::SetTabUp
+            | OpCode::SetTable
+            | OpCode::SetI
+            | OpCode::SetField
+            | OpCode::Self_
+            | OpCode::TailCall
+            | OpCode::Return => writeln!(
+                w,
+                "{} {} {}{}",
+                insn.a(),
+                insn.b(),
+                insn.c(),
+                if insn.k() { "k" } else { "" }
+            )?,
+        };
+    }
+
+    if verbose {
+        writeln!(w, "constants ({}):", proto.constants.len())?;
+        for (i, constant) in proto.constants.iter().enumerate() {
+            write!(w, "\t{}\t", i)?;
+            w.write_all(match constant {
+                Value::Nil => b"N",
+                Value::Boolean(_) => b"B",
+                Value::Integer(_) => b"I",
+                Value::Number(_) => b"F",
+                Value::String(_) => b"S",
+                _ => unreachable!(),
+            })?;
+            match constant {
+                Value::Nil => w.write_all(b"\tnil\n")?,
+                Value::Boolean(b) => writeln!(w, "\t{}", b)?,
+                Value::Integer(i) => writeln!(w, "\t{}", i)?,
+                Value::Number(x) => writeln!(w, "\t{}", x)?,
+                Value::String(s) => writeln!(w, "\t\"{}\"", s.as_bstr())?,
+                _ => unreachable!(),
+            };
+        }
+
+        writeln!(w, "upvalues ({}):", proto.upvalues.len())?;
+        for (i, desc) in proto.upvalues.iter().enumerate() {
+            write!(w, "\t{}\t", i)?;
+            match desc {
+                UpvalueDescription::Register(i) => writeln!(w, "1\t{}", i.0)?,
+                UpvalueDescription::Upvalue(i) => writeln!(w, "0\t{}", i.0)?,
+            }
+        }
+    }
+
+    writeln!(w)?;
+
+    for proto in proto.protos.iter() {
+        dump_proto(w, proto, verbose)?;
+    }
+
     Ok(())
 }
